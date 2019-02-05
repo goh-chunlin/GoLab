4062c811b82768eaaf67578f64ae40f10ac24398
goh-chunlin <chunlin@live.com.sg>
2019-02-06 02:11:38 +0800

    Corrected the content type of static files.

--- staticFile.go
+++ staticFile.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"mime"
 	"net/http"
+	"strings"
 )
 
 func staticFile(writer http.ResponseWriter, request *http.Request) {
 	staticFiles := http.FileServer(http.Dir("public"))
 	http.StripPrefix("/static/", staticFiles)
+
+	urlComponents := strings.Split(request.URL.Path, ".")
+	fileExtension := urlComponents[len(urlComponents)-1]
+
+	writer.Header().Set("Content-Type", mime.TypeByExtension(fileExtension))
 }
