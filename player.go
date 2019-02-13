b36a4728b52186290621be0c9b2452780e611780
goh-chunlin <chunlin@live.com.sg>
2019-02-13 18:20:34 +0800

    Corrected the multiple response.WriteHeader calls issue.

--- player.go
+++ player.go
@@ -8,10 +8,14 @@ import (
 func player(writer http.ResponseWriter, request *http.Request) {
 	user := profileFromSession(request)
 	if user == nil {
+
 		http.Redirect(writer, request, "/", http.StatusFound)
+
+	} else {
+
+		http.ServeFile(writer, request, "templates/player.html")
+
+		writer.Header().Set("Content-Type", mime.TypeByExtension("html"))
+
 	}
-
-	http.ServeFile(writer, request, "templates/player.html")
-
-	writer.Header().Set("Content-Type", mime.TypeByExtension("html"))
 }
