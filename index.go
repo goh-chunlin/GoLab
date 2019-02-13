2ed6fd539dbf07b5820c792bad968b2b887c9bd2
goh-chunlin <chunlin@live.com.sg>
2019-02-13 16:17:18 +0800

    Removed testing code.

--- index.go
+++ index.go
@@ -1,28 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"mime"
 	"net/http"
-
-	"github.com/goh-chunlin/GoLab/models"
 )
 
 func index(writer http.ResponseWriter, request *http.Request) {
 	http.ServeFile(writer, request, "templates/index.html")
 
 	writer.Header().Set("Content-Type", mime.TypeByExtension("html"))
-}
-
-func indexWithJSON(writer http.ResponseWriter, request *http.Request) {
-	video, err := models.GetVideo(8)
-	checkError(err)
-
-	output, err := json.MarshalIndent(&video, "", "\t\t")
-	checkError(err)
-
-	writer.Header().Set("Content-Type", "application/json")
-	writer.Write(output)
-
-	return
 }
