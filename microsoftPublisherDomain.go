69183599371fae5492c5c426d0dcbd27f7303902
goh-chunlin <chunlin@live.com.sg>
2019-06-25 22:48:41 +0800

    Changed to use byte[].

--- microsoftPublisherDomain.go
+++ microsoftPublisherDomain.go
@@ -5,7 +5,15 @@ import (
 )
 
 func microsoftPublisherDomain(writer http.ResponseWriter, request *http.Request) {
-	http.ServeFile(writer, request, ".well-known/microsoft-identity-association.json")
+	jsonData := []byte(`{
+		"associatedApplications": [
+		  {
+			"applicationId": "332f4102-5c40-4f80-a70e-5023184125a1"
+		  }
+		]
+	  }`)
 
+	writer.WriteHeader(200)
 	writer.Header().Set("Content-Type", "application/json")
+	writer.Write(jsonData)
 }
