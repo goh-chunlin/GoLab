07f47fea866f0054deb2722bc5f2613250e56307
goh-chunlin <chunlin@live.com.sg>
2019-06-25 22:30:29 +0800

    Fixed the error "urlComponents declared and not used".

--- microsoftPublisherDomain.go
+++ microsoftPublisherDomain.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"net/http"
-	"strings"
 )
 
 func microsoftPublisherDomain(writer http.ResponseWriter, request *http.Request) {
-	urlComponents := strings.Split(request.URL.Path, "/")
-
 	http.ServeFile(writer, request, ".well-known/microsoft-identity-association.json")
 
 	writer.Header().Set("Content-Type", "application/json")
