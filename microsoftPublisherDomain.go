824744088179023292f7a1d72a3ad6a78580fc01
goh-chunlin <chunlin@live.com.sg>
2019-06-26 00:32:14 +0800

    Changed back to use ServeFile for JSON.

--- microsoftPublisherDomain.go
+++ microsoftPublisherDomain.go
@@ -5,15 +5,7 @@ import (
 )
 
 func microsoftPublisherDomain(writer http.ResponseWriter, request *http.Request) {
-	jsonData := []byte(`{
-		"associatedApplications": [
-		  {
-			"applicationId": "332f4102-5c40-4f80-a70e-5023184125a1"
-		  }
-		]
-	  }`)
-
-	writer.WriteHeader(200)
 	writer.Header().Set("Content-Type", "application/json")
-	writer.Write(jsonData)
+
+	http.ServeFile(writer, request, ".well-known/microsoft-identity-association.json")
 }
